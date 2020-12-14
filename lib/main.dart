01b1f8b0817b5d2f566dd4bd05fabe4ce850721e
teppo223 <t6hote00@students.oamk.fi>
2020-12-14 12:39:59 +0200

    Saunamittari tehty valmiiksi

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show C
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:numberpicker/numberpicker.dart';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/cupertino.dart';
+
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +33,7 @@ class MyApp extends StatelessWidget {
         routes: {
         '/': (context) => MyHomePage(),
         //'/second': (context) => SecondRoute(),  //Piti laittaa kommenttiin että 'onGenerateRoute' toimii oikein ja antaa ID:n eteenpäin
-        '/third': (context) => ThirdRoute(),
+        //'/third': (context) => ThirdRoute(),
         },
       theme: ThemeData(
         // This is the theme of your application.
@@ -49,6 +53,16 @@ class MyApp extends StatelessWidget {
           return MaterialPageRoute(
             builder: (context) {
               return SecondRoute(
+                  id: args.id
+              );
+            },
+          );
+        }
+        if (settings.name == ThirdRoute.routeName) {
+          final PassID args = settings.arguments;
+          return MaterialPageRoute(
+            builder: (context) {
+              return ThirdRoute(
                   id: args.id
               );
             },
@@ -87,6 +101,22 @@ class _MyHomePageState extends State<MyHomePage> {
   List<String> deviceList = new List<String>();
   String idChoice;
 
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+  var initializationSettingsAndroid;
+  var initializationSettingsIOS;
+  var initializationSettings;
+
+  /*void showNotification() async{
+    await demoNotification();
+  }
+
+  Future<void> demoNotification() async{
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails('channel_ID', 'channel name', 'channel description', importance: Importance.Max, priority: Priority.High, ticker: 'test ticker');
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(androidPlatformChannelSpecifics, iOSChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, 'Saunamittari', 'Saunasi on nyt valmis!', platformChannelSpecifics, payload: 'test payload');
+  }*/
+
   @override
   void initState(){
     super.initState();
@@ -98,6 +128,42 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     });
     FlutterBlue.instance.startScan();
+
+    initializationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+    initializationSettingsIOS = new IOSInitializationSettings(
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initializationSettings = new InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) async{
+    if(payload != null){
+      debugPrint('notification payload: $payload');
+    }
+    await Navigator.push(context,
+    new MaterialPageRoute(builder: (context)=>ThirdRoute()));
+  }
+
+  Future onDidReceiveLocalNotification(int id, String body, String title, String payload) async{
+    await showDialog(
+      context: context,
+      builder: (BuildContext context)=>CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: Text('OK'),
+            onPressed: () async{
+              Navigator.of(context, rootNavigator: true).pop();
+              await Navigator.push(context,
+              MaterialPageRoute(builder: (context)=>ThirdRoute()));
+            },
+          )
+        ],
+      )
+    );
   }
 
   addToList(String id) {
@@ -165,8 +231,15 @@ class _MyHomePageState extends State<MyHomePage> {
             RaisedButton(
               child: Text('Saunamittari'),
               onPressed: () {
-                FlutterBlue.instance.stopScan();
-                Navigator.pushNamed(context, '/third');
+                if (idChoice == null){
+                  alertWindow();
+                } else {
+                  FlutterBlue.instance.stopScan();
+                  print('Passing ID:');
+                  print(idChoice);
+                  Navigator.pushReplacementNamed(
+                      context, '/third', arguments: PassID(idChoice));
+                }
               },
             ),
             for (String id in deviceList) new ListTile(
@@ -413,7 +486,8 @@ void bluetoothIsolate(SendPort fromIsolate) {
 }
 
 class ThirdRoute extends StatefulWidget{
-  ThirdRoute({Key key}) : super(key: key);
+  static const routeName = '/third';
+  ThirdRoute({Key key, this.id}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -428,37 +502,187 @@ class ThirdRoute extends StatefulWidget{
   //final FlutterBlue flutterBlue = FlutterBlue.instance;
   //final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
 
+  final String id;
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+
   @override
   _ThirdRouteState createState() => _ThirdRouteState();
 }
 
 class _ThirdRouteState extends State<ThirdRoute> {
+  double tempC = 0;
+  bool notScanningYet = true;
+  Isolate isolate;
+
   double _currentValue = 1.0;
+  int page = 1;
+  bool isCheckOn = false;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+  Timer timer;
+
+  @override
+  void initState(){
+    super.initState();
+    /*
+    widget.flutterBlue.startScan(timeout: Duration(seconds: 3));
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        if (result.device.id.toString().contains(widget.id)) {
+          print(result.advertisementData.manufacturerData);
+          parseManufacturerData(result.advertisementData.manufacturerData);
+          setState(() {});
+        }
+      }
+    });
+    */
+    initIsolate();
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => temperatureCheck());
+  }
+
+  Future<void> _incrementCounter() async {
+    /*
+    SendPort toIsolate = await initIsolate();
+    toIsolate.send('Isolate started');
+    print('ID is this:');
+    print(widget.id);
+
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+    */
+  }
+
+  parseManufacturerData(data) {
+    var manufacturerData = Uint8List.fromList(data[1177]);
+    var pressure = ByteData.sublistView(manufacturerData, 4, 6);
+    var humidity = ByteData.sublistView(manufacturerData, 2, 4);
+    var temperature = ByteData.sublistView(manufacturerData, 0, 2);
+    print("Pressure: ${(pressure.getUint16(0, Endian.little)+50000)/100} hPa");
+    print("Humidity: ${humidity.getUint16(0, Endian.little)/400} %");
+    print("Temperature: ${temperature.getUint16(0, Endian.little)*0.005} \u{00B0}C");
+    tempC = temperature.getUint16(0, Endian.little)*0.005;
+    PushData(widget.id, tempC).addValue();
+  }
+
+  Future<SendPort> initIsolate() async {
+    Completer completer = new Completer<SendPort>();
+    ReceivePort fromIsolate = ReceivePort();
+
+    fromIsolate.listen((data) {
+      if (data is SendPort) {
+        SendPort toIsolate = data;
+        completer.complete(toIsolate);
+      } else {
+        print(data);
+        widget.flutterBlue.startScan(timeout: Duration(seconds: 3));
+        widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+          if(notScanningYet){
+            notScanningYet = false;
+            for (ScanResult result in results) {
+              //'D9:E5:26:B2:B0:09' : 'E6:C0:0A:82:3C:3F' : 'E4:FA:5E:EE:BF:D8'   Just in case vielä ID:t tallessa nopeasti saatavilla
+              if (result.device.id.toString().contains(widget.id)) {
+                print(result.advertisementData.manufacturerData);
+                parseManufacturerData(result.advertisementData.manufacturerData);
+                setState(() {});
+              }
+            }
+          }
+        });
+        notScanningYet = true;
+      }
+    });
+
+    isolate = await Isolate.spawn(bluetoothIsolate, fromIsolate.sendPort);
+    return completer.future;
+  }
+
+  void temperatureCheck(){
+    if(page == 0 && isCheckOn) {
+      if (tempC >= _currentValue) {
+        showNotification();
+        isCheckOn = false;
+      }
+    }
+  }
+
+  void showNotification() async{
+    await demoNotification();
+  }
+
+Future<void> demoNotification() async{
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails('channel_ID', 'channel name', 'channel description', importance: Importance.Max, priority: Priority.High, ticker: 'test ticker');
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(androidPlatformChannelSpecifics, iOSChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, 'Saunamittari', 'Saunasi on nyt valmis!', platformChannelSpecifics, payload: 'test payload');
+}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Saunamittari'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text("Valitse lämpötila"),
-            NumberPicker.decimal(
-                initialValue: _currentValue,
-                minValue: 0,
-                maxValue: 100,
-                onChanged: (newValue) =>
-                    setState(() => _currentValue = newValue)),
-             Text("Current number: $_currentValue"),
-          ],
+    if(page == 1) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saunamittari'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("Valitse lämpötila"),
+              NumberPicker.decimal(
+                  initialValue: _currentValue,
+                  minValue: 0,
+                  maxValue: 100,
+                  onChanged: (newValue) =>
+                      setState(() => _currentValue = newValue)),
+              Text("Olet valinnut: $_currentValue astetta."),
+              RaisedButton(
+                child: Text('Käynnistä'),
+                onPressed: () {
+                  setState(() {
+                    page = 0;
+                    isCheckOn = true;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    else{
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saunamittari'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('$tempC \u{00B0}C/$_currentValue \u{00B0}C', style: Theme.of(context).textTheme.headline4,),
+              RaisedButton(
+                child: Text('Peruuta'),
+                onPressed: () {
+                  setState(() {
+                    page = 1;
+                    _currentValue = 1.0;
+                    isCheckOn = false;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
 
